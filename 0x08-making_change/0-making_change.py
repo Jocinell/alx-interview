0e2e7b3a6c45e47ee366ce4338bdf2ea5bec771c
Aysanew Yonas <aysanewyonasj@gmail.com>
2023-03-24 20:21:14 +0300

    fix execution

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,34 +1,43 @@
 #!/usr/bin/python3
-"""Module used to add two arrays."""
+"""
+Making Change
+"""
 
 
 def makeChange(coins, total):
-    """[Given a pile of coins of different values, determine the fewest number
-            of coins needed to meet a given amount total]
-    Args:
-            coins ([list]): [list of the values of your the coins]
-                              The value of a coin will always be an int > 0
-            total ([type]): [description]
-    Returns:
-            c [int]: (change  [fewest number of coins needed to meet total]
     """
-
+    Return the minimum number of coins needed to meet a given total
+    Args:
+        coins (list of ints): a list of coins of different values
+        total (int): total value to be met
+    Return:
+        Number of coins or -1 if meeting the total is not possible
+    """
     if total <= 0:
         return 0
-
-    # verify coins is a valid
-    if (coins is None or len(coins) == 0):
+    if coins == [] or coins is None:
         return -1
+    try:
+        n = coins.index(total)
+        return 1
+    except ValueError:
+        pass
 
-    change = 0
-    my_coins = sorted(coins, reverse=True)
-    money_left = total
-
-    for coin in my_coins:
-        while (money_left % coin >= 0 and money_left >= coin):
-            change += int(money_left / coin)
-            money_left = money_left % coin
-
-    change = change if money_left == 0 else -1
-
-    return change
+    coins.sort(reverse=True)
+    coin_count = 0
+    for i in coins:
+        if total % i == 0:
+            coin_count += int(total / i)
+            return coin_count
+        if total - i >= 0:
+            if int(total / i) > 1:
+                coin_count += int(total / i)
+                total = total % i
+            else:
+                coin_count += 1
+                total -= i
+                if total == 0:
+                    break
+    if total > 0:
+        return -1
+    return coin_count
